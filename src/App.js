4cbb03ee5c355208da43a3e95b71a64e0b41dc57
ameeralih <ameerali.hassanali@gmail.com>
2022-10-09 01:07:05 +0800

    Generate 16 random letters into 4 rows. Add form below

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
 
-class App extends React.Component {
+class BoggleBoard extends React.Component {
+  generateString() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    const charactersLength = characters.length;
+    result = characters.charAt(Math.floor(Math.random() * charactersLength));
+    return result;
+  }
+
+  generateBoard() {
+    const row1 = [];
+    const row2 = [];
+    const row3 = [];
+    const row4 = [];
+    for (let i = 0; i < 4; i++) {
+      row1.push(this.generateString());
+    }
+    for (let i = 0; i < 4; i++) {
+      row2.push(this.generateString());
+    }
+    for (let i = 0; i < 4; i++) {
+      row3.push(this.generateString());
+    }
+    for (let i = 0; i < 4; i++) {
+      row4.push(this.generateString());
+    }
+    return (
+      <div className="board">
+        <div className="row">{row1}</div>
+        <div className="row">{row2}</div>
+        <div className="row">{row3}</div>
+        <div className="row">{row4}</div>
+      </div>
+    );
+  }
+
+  render() {
+    return <div>{this.generateBoard()}</div>;
+  }
+}
+
+class UserInput extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <form>
+        <input type="text" />
+      </form>
     );
   }
 }
 
-export default App;
+export class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Boggle Board</p>
+        <BoggleBoard />
+        <UserInput />
+      </div>
+    );
+  }
+}
