58455a877840d8c512794fbc213bcff3c885812e
ameeralih <ameerali.hassanali@gmail.com>
2022-10-17 21:20:59 +0800

    add timer

--- src/App.js
+++ src/App.js
@@ -9,9 +9,24 @@ class App extends React.Component {
       board: this.generateBoard(),
       guess: "",
       dictionary: require("an-array-of-english-words"),
+      wordInOrNotMessage: "",
+      wordsGuessed: [],
+      correctWordsGuessed: [],
+      score: 0,
+      gameStart: false,
+      secondsToGameEnd: 180,
     };
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  tick() {
+    this.setState({
+      secondsToGameEnd: this.state.secondsToGameEnd - 1,
+    });
+  }
   generateLetter() {
     const characters = "abcdefghijklmnopqrstuvwxyz";
     let result = "";
@@ -23,17 +38,21 @@ class App extends React.Component {
     const row1 = [];
     const row2 = [];
     const row3 = [];
+    const row4 = [];
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
       row1.push(this.generateLetter());
     }
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
       row2.push(this.generateLetter());
     }
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
       row3.push(this.generateLetter());
     }
-    const board = [row1, row2, row3];
+    for (let i = 0; i < 4; i++) {
+      row4.push(this.generateLetter());
+    }
+    const board = [row1, row2, row3, row4];
     return board;
   }
 
@@ -98,16 +117,25 @@ class App extends React.Component {
           <div className="cell">{boardArray[0][0]}</div>
           <div className="cell">{boardArray[0][1]}</div>
           <div className="cell">{boardArray[0][2]}</div>
+          <div className="cell">{boardArray[0][3]}</div>
         </div>
         <div>
           <div className="cell">{boardArray[1][0]}</div>
           <div className="cell">{boardArray[1][1]}</div>
           <div className="cell">{boardArray[1][2]}</div>
+          <div className="cell">{boardArray[1][3]}</div>
         </div>
         <div>
           <div className="cell">{boardArray[2][0]}</div>
           <div className="cell">{boardArray[2][1]}</div>
           <div className="cell">{boardArray[2][2]}</div>
+          <div className="cell">{boardArray[2][3]}</div>
+        </div>
+        <div>
+          <div className="cell">{boardArray[3][0]}</div>
+          <div className="cell">{boardArray[3][1]}</div>
+          <div className="cell">{boardArray[3][2]}</div>
+          <div className="cell">{boardArray[3][3]}</div>
         </div>
       </div>
     );
@@ -116,8 +144,27 @@ class App extends React.Component {
   handleSubmit(e) {
     const dictionary = this.state.dictionary;
     e.preventDefault();
+    console.log(this.state.guess);
     console.log(dictionary.includes(this.state.guess));
     console.log(this.checkWord(this.state.board, this.state.guess));
+    if (
+      dictionary.includes(this.state.guess) &&
+      this.checkWord(this.state.board, this.state.guess)
+    ) {
+      console.log("yes");
+      this.setState({
+        wordInOrNotMessage: "Yes!",
+        correctWordsGuessed: [
+          ...this.state.correctWordsGuessed,
+          this.state.guess,
+        ],
+      });
+    } else {
+      console.log("no");
+      this.setState({
+        wordInOrNotMessage: "No!",
+      });
+    }
   }
 
   handleChange(e) {
@@ -128,17 +175,27 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="board">
-        {this.board()}
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          <input
-            type="text"
-            name="guess"
-            value={this.state.guess}
-            onChange={(e) => this.handleChange(e)}
-          />
-          <button>Enter</button>
-        </form>
+      <div>
+        <div className="board">
+          {this.board()}
+          <form onSubmit={(e) => this.handleSubmit(e)}>
+            <input
+              type="text"
+              name="guess"
+              value={this.state.guess}
+              onChange={(e) => this.handleChange(e)}
+            />
+            <button>Enter</button>
+          </form>
+        </div>
+        <p>Time Left: {this.state.secondsToGameEnd} seconds</p>
+        <div className="correctWords">
+          <p>
+            <b>Correct Words</b>
+          </p>
+
+          <p>{this.state.correctWordsGuessed}</p>
+        </div>
       </div>
     );
   }
